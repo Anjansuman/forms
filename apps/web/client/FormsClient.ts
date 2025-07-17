bd4c2afd9edbe28efa3592b9867d77e17ec31d4e
Anjan Suman <anjansuman80@gmail.com>
2025-07-17 12:23:18 +0530

    wrote abstraction of form interaction.

--- apps/web/client/FormsClient.ts
+++ apps/web/client/FormsClient.ts
@@ -9,7 +9,7 @@ import { FormField } from "../types/FormField";
 
 export class FormsClient {
 
-    private program: Program <FormsContract>| null = null;
+    private program: Program<FormsContract> | null = null;
     private provider: AnchorProvider | null = null;
     private wallet: WalletContextState | null = null;
 
@@ -24,39 +24,141 @@ export class FormsClient {
     }
 
     private init(connection: Connection, wallet: WalletContextState) {
-        this.wallet = wallet;
+        try {
+            this.wallet = wallet;
 
-        this.provider = new AnchorProvider(
-            connection,
-            wallet as any,
-            {
-                commitment: "confirmed"
-            }
-        );
-        this.program = new Program<FormsContract>(idl as FormsContract, this.provider);
+            this.provider = new AnchorProvider(
+                connection,
+                wallet as any,
+                {
+                    commitment: "confirmed"
+                }
+            );
+            this.program = new Program<FormsContract>(idl as Idl, this.provider);
+        } catch (error) {
+            this.handleError(error);
+        }
     }
 
-    public async createForm(formId: number, title: string, description: string, schema: FormField[]): Promise<void> {
+    public async createForm(formId: number, title: string, description: string, schema: FormField[]): Promise<string | null> {
         try {
 
-            if(!this.program) {
-                throw new Error("Program not found!");
+            if (!this.program) {
+                this.handleError("Program not found!");
+                return null;
             }
 
             const [formPDA, bump] = this.getFormPda(formId, this.getWalletPublicKey());
 
             const res = await this.program.methods
-            .createForm(new BN(formId), title, description, schema)
+                .createForm(new BN(formId), title, description, schema)
+                .accountsStrict({
+                    formMetadata: formPDA,
+                    creator: this.getWalletPublicKey(),
+                    systemProgram: SystemProgram.programId
+                })
+                .signers([])
+                .rpc();
+
+            // return the transaction
+            return res;
+
+            /*
+            
+            there is this, [this can be used to auto create pda or other accounts]
             .accounts({
+                creator: this.getWalletPublickey()
+            })
+            
+            */
+
+        } catch (error) {
+            this.handleError(error);
+            return null;
+        }
+    }
+
+    public async submitResponse(formId: number,formCreator: PublicKey, answers: Uint8Array): Promise<string | null> {
+        try {
+
+            if (!this.program) {
+                this.handleError("Program not found!");
+                return null;
+            }
+
+            const [responsePDA, bump] = this.getResponsePda(formId, this.getWalletPublicKey());
+            const [formPDA, _] = this.getFormPda(formId, formCreator);
+
+            const res = await this.program.methods
+            .submitResponse(new BN(formId), Buffer.from(answers))
+            .accountsStrict({
                 formMetadata: formPDA,
-                creator: this.getWalletPublicKey(),
+                responseAccount: responsePDA,
+                responder: this.getWalletPublicKey(),
                 systemProgram: SystemProgram.programId
             })
             .signers([])
             .rpc()
 
-        } catch (error) {
+            return res;
 
+        } catch (error) {
+            this.handleError(error);
+            return null;
+        }
+    }
+
+    public async close_form(formId: number, formCreator: PublicKey): Promise<string | null> {
+        try {
+
+            if(!this.program) {
+                this.handleError("Program not found!");
+                return null;
+            }
+
+            const [formPDA, bump] = this.getFormPda(formId, formCreator);
+
+            const res = await this.program.methods
+            .closeForm(new BN(formId))
+            .accountsStrict({
+                formMetadata: formPDA,
+                creator: formCreator
+            })
+            .signers([])
+            .rpc();
+
+            return res;
+            
+        } catch (error) {
+            this.handleError(error);
+            return null;
+        }
+    }
+
+    public async delete_response(formId: number, formCreator: PublicKey): Promise<string | null> {
+        try {
+
+            if(!this.program) {
+                this.handleError("Program not found!");
+                return null;
+            }
+
+            const [responsePDA, bump] = this.getResponsePda(formId, this.getWalletPublicKey());
+
+            const res = await this.program.methods
+            .deleteResponse(new BN(formId))
+            .accountsStrict({
+                responseAccount: responsePDA,
+                responder: this.getWalletPublicKey()
+            })
+            .signers([])
+            .rpc();
+
+            return res;
+            
+        } catch (error) {
+            this.handleError(error);
+            return null;
         }
     }
 
@@ -102,6 +204,19 @@ export class FormsClient {
         }
 
         return this.wallet.publicKey!;
+    }
+
+    private handleError(error: unknown): void {
+        if (typeof error == undefined) {
+            throw new Error("undefined error!");
+        }
+
+        if (typeof error === "string") {
+            throw new Error(error);
+        }
+
+        throw new Error("unknown error!");
+
     }
 
 }
